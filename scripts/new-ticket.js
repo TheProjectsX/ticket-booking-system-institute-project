433130783e03081ea0f40139507843813d9e2818
Modasser Hasan <manvstech2.0@gmail.com>
2024-11-09 00:50:43 +0600

    Ticket Selection option added

--- scripts/new-ticket.js
+++ scripts/new-ticket.js
@@ -1,3 +1,12 @@
+const elm_findTicketsContainer = document.getElementById(
+    "findTicketsContainer"
+);
+const elm_findTicketsForm = document.getElementById("findTickets");
+const elm_showBussContainer = document.getElementById("showBussContainer");
+
+const elm_purchaseTicketContainer = document.getElementById(
+    "purchaseTicketContainer"
+);
 const elm_seatOptions = document.querySelectorAll(".seat");
 const elm_totalPriceItems = document.querySelectorAll(".totalPrice");
 const elm_selectedSeatContainer = document.getElementById(
@@ -11,9 +20,9 @@ const elm_error = document.getElementById("error");
 const elm_modal = document.getElementById("checkoutModal");
 const elm_checkoutInfo = document.getElementById("checkoutInfo");
 
-const elm_route = document.getElementById("route");
-const elm_date = document.getElementById("date");
-const elm_time = document.getElementById("time");
+const elm_finalRoute = document.getElementById("route");
+const elm_finalDate = document.getElementById("date");
+const elm_finalTime = document.getElementById("time");
 
 const elm_modalCloseBtn = document.getElementById("closeBtn");
 const elm_printBtn = document.getElementById("printBtn");
@@ -22,11 +31,79 @@ const elm_printBtn = document.getElementById("printBtn");
 let selectedSeats = [];
 const seatPrice = 550;
 
+const busTimes = ["12:00 PM", "10:00 PM"];
+
+const findTicketHTML = `<button
+                    class="selectBus flex flex-col shadow-lg border-2 border-[dodgerBlue] w-96 p-4 rounded-lg active:scale-95 transition-transform duration-200 ease-in-out"
+                    data-route="$Route"
+                    data-time="$Time"
+                    data-date="$Date"
+                    onclick="selectTicketHandler(this)"
+                >
+                    <h4 class="flex justify-between w-full">
+                        <span class="font-semibold">Route:</span>
+                        <span>$Route</span>
+                    </h4>
+                    <p class="flex justify-between w-full">
+                        <span class="font-semibold">Time:</span>
+                        <span>$Time</span>
+                    </p>
+                    <p class="flex justify-between w-full">
+                        <span class="font-semibold">Date:</span>
+                        <span>$Date</span>
+                    </p>
+                </button>`;
+
 const selectedSeatHTML = `<div class="flex justify-between">
 <p class="text-[#03071299]">SelectedSeatNo</p>
 <p class="text-[#03071299]">Regular</p>
 <p class="text-[#03071299]">${seatPrice}</p>
 </div>`;
+
+function getDateFromToday(days) {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + days);
+
+    // Format to "Oct 5, 2024"
+    const options = { month: "short", day: "numeric", year: "numeric" };
+    const formattedDate = tomorrow.toLocaleDateString("en-US", options);
+
+    return formattedDate;
+}
+
+const selectTicketHandler = (e) => {
+    const route = e.dataset.route;
+    const time = e.dataset.time;
+    const date = e.dataset.date;
+
+    elm_finalRoute.innerText = route;
+    elm_finalTime.innerText = time;
+    elm_finalDate.innerText = date;
+
+    elm_findTicketsContainer.classList.replace("flex", "hidden");
+    elm_purchaseTicketContainer.classList.replace("hidden", "flex");
+};
+
+// Find Tickets
+elm_findTicketsForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    elm_showBussContainer.innerHTML = "";
+
+    const form = e.target;
+    const departure = form.departure.value;
+    const destination = form.destination.value;
+
+    for (let i = 0; i < 2; i++) {
+        const element = findTicketHTML
+            .replaceAll("$Route", `${departure}-${destination}`)
+            .replaceAll("$Time", busTimes[i])
+            .replaceAll("$Date", getDateFromToday(i + 1));
+
+        elm_showBussContainer.innerHTML += element;
+    }
+});
 
 // Add event Listener to every Seat
 
@@ -95,10 +172,12 @@ elm_purchaseTicketForm.addEventListener("submit", (e) => {
         <p><strong>Seats:</strong> ${selectedSeats
             .map((item) => item.innerText)
             .join(", ")}</p>
-        <p><strong>From:</strong> ${elm_route.innerText
+        <p><strong>From:</strong> ${elm_finalRoute.innerText
             .split("-")[0]
             .trim()}</p>
-        <p><strong>To:</strong> ${elm_route.innerText.split("-")[1].trim()}</p>
+        <p><strong>To:</strong> ${elm_finalRoute.innerText
+            .split("-")[1]
+            .trim()}</p>
         <p><strong>Date of Journey:</strong> ${date.innerText}</p>
         <p><strong>Time of Journey:</strong> ${time.innerText}</p>
         <p><strong>Ticket Type:</strong> Regular</p>
@@ -111,8 +190,8 @@ elm_purchaseTicketForm.addEventListener("submit", (e) => {
         number,
         txId,
         seats: selectedSeats.map((item) => item.innerText).join(", "),
-        from: elm_route.innerText.split("-")[0].trim(),
-        to: elm_route.innerText.split("-")[1].trim(),
+        from: elm_finalRoute.innerText.split("-")[0].trim(),
+        to: elm_finalRoute.innerText.split("-")[1].trim(),
         date: date.innerText,
         time: time.innerText,
         total: elm_totalPriceItems[0].innerText,
@@ -134,6 +213,8 @@ elm_printBtn.addEventListener("click", () => {
     window.print();
 });
 
+// Run on Load
+
 const localData = JSON.parse(localStorage.getItem("ticket"));
 if (localData) {
     const seats = localData.seats.split(", ");
@@ -144,13 +225,3 @@ if (localData) {
         }
     });
 }
-
-const today = new Date();
-const tomorrow = new Date(today);
-tomorrow.setDate(today.getDate() + 1);
-
-// Format to "Oct 5, 2024"
-const options = { month: "short", day: "numeric", year: "numeric" };
-const formattedDate = tomorrow.toLocaleDateString("en-US", options);
-
-elm_date.innerText = formattedDate;
